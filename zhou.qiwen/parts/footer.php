1ba22076cd8d7c35cb3eee95b5a38c721b7b7b0f
qiwen zhou <qiwenzhou@qiwendeMacBook-Pro.local>
2020-10-25 18:18:49 -0700

    add parts folder

---
+++ zhou.qiwen/parts/footer.php
@@ -0,0 +1,6 @@
+<div class="footer">
+	<div class="">
+		<div class=''>Copyright ZhouQiwen</div>
+		<div class='' href="zhouqiwen19801@gmail.com">zhouqiwen19801@gmail.com</div>
+	</div>
+</div>
---
+++ zhou.qiwen/parts/navbar.php
@@ -0,0 +1,33 @@
+<header class="navbar">
+   <div class="container display-flex flex-align-center">
+      <div class="flex-none"><h1>Car Heaven</h1></div>
+      <div class="flex-stretch"></div>
+      
+      <!-- nav.nav>ul>li*4>a[href=#article$]>{Link $} -->
+      <nav class="nav flex-none">
+         <ul class="display-flex">
+            <li><a href="./index.php">Home</a></li>
+            <li><a href="./products.php">Search Cars</a></li>
+            <li><a href="./index.php">Trade/Sale</a></li>
+			<li><a href="./index.php">Help</a></li>
+         </ul>
+      </nav>
+   </div>
+</header>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
