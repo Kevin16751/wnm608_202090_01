ae6ff0936b8a19c39953b4ddff7679b0be2747bd
qiwen zhou <qiwenzhou@qiwendeMacBook-Pro.local>
2020-10-25 21:30:36 -0700

    pds php

--- zhou.qiwen/styleguide/index.php
+++ zhou.qiwen/styleguide/index.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-   <title>Store</title>
+   <title>Home</title>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-   <?php include "parts/meta.php" ?>
+   <?php include "../parts/meta.php" ?>
 </head>
 <body>
    
-   <?php include "parts/navbar.php" ?>
+   <?php include "../parts/navbar.php" ?>
 
 
 
-   <div class="view-window" style="background-image:url(img/car_001.jpg)">
+   <div class="view-window" style="background-image:url(../img/car_001.jpg)">
       <h2>Product Name</h2>
    </div>
    <div class="container">
---
+++ zhou.qiwen/styleguide/products_list.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <title>Product List</title>
+
+   <?php include "../parts/meta.php" ?>
+</head>
+<body>
+   
+   <?php include "../parts/navbar.php" ?>
+
+   <?php include "../parts/products.php" ?>
+
+   <?php include "../parts/footer.php" ?>
+
+   
+
+</body>
+</html>
